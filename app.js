2348487ce55aba26148b6d737997dc733a6aa1c6
Nika <nabakhtevelin@gmail.com>
2021-07-19 17:38:23 +0400

    latest stable version

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const hostname2 = "http://127.0.0.1:3000";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const urlencodedParser = bodyParser.urlencoded({extended: false });
 app.set("view engine", "pug");
 
 app.use("/assets", express.static("assets"));
@@ -37,7 +37,8 @@ app.get("/", (req, res) => {
 app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
   res.render(__dirname + "/snippet/post-success", {
     data: req.body, 
-    image: `./assets/serverImages/${req.file.originalname}`
+    image: `./assets/serverImages/${req.file.originalname}`,
+    generatedImage: "image.png"
   });
 
   nodeHtmlToImage({
@@ -45,93 +46,119 @@ app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
     html: `
     <html>
     <style>
-    @import url("//cdn.web-fonts.ge/fonts/bpg-nino-mtavruli/css/bpg-nino-mtavruli.min.css");
+      @import url("//cdn.web-fonts.ge/fonts/bpg-nino-mtavruli/css/bpg-nino-mtavruli.min.css");
 
-    body {
-      overflow: hidden;
-    }
-    
-    .post-container {
-      width: 2048px;
-      height: 2048px;
-      transform-origin: top left;
-      margin: 50px 0;
-      left: 50%;
-      transform: scale(0.25) translateX(-50%);
-      font-family: "BPG Nino Mtavruli", sans-serif;
-    
-      text-align: center;
-      position: relative;
-      color: white;
-      border: 50px solid #08a53f;
-    }
-    
-    .content {
-      margin-top: -45px;
-    }
-    
-    .img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-    
-    .text {
-      width: 100%;
-      height: 90%;
-      position: absolute;
-      left: 0;
-      top: 0;
-      display: flex;
-      justify-content: center;
-      align-items: flex-end;
-    }
-    
-    .text p {
-      width: 1620px;
-      font-weight: bold;
-      font-size: 130px;
-      line-height: 150px;
-    }
-    
-    .author {
-      position: absolute;
-      bottom: 0;
-      left: 50%;
-      transform: translate(-50%, -50%);
-    
-      font-size: 76px;
-      line-height: 87px;
-    }
-    
-    .author::after {
-      content: "";
-      position: absolute;
-      bottom: 0;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      height: 10px;
-      width: 100%;
-      background-color: #08a53f;
-      border-radius: 21px;
-    }
-    
-    #btn {
-      margin: 20px 0 0 20px;
-    }
+      body {
+        overflow: hidden;
+        width: 2048px;
+        height: 2048px;
+      }
+
+      .post-container {
+        width: 2048px;
+        height: 2048px;
+        transform-origin: top left;
+        margin: 50px 0;
+        left: 50%;
+        transform: translateX(-50%);
+        font-family: "BPG Nino Mtavruli Bold", sans-serif;
+        text-align: center;
+        position: relative;
+        color: white;
+        overflow: hidden;
+      }
+
+      .post-container::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        background: url("http://127.0.0.1:3000/assets/post/img/post-bg.png") no-repeat center/cover,  linear-gradient(
+          1deg
+          , #000 0%, transparent 40%);
+        z-index: 10;
+        filter: blur(2px);
+        pointer-events: none;
+      }
+
+      .content {
+        margin-top: -45px;
+      }
+
+      .img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+
+      .text {
+        width: 100%;
+        height: 90%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+        z-index: 100;
+
+      }
+
+      .text p {
+        width: 1565px;
+        font-weight: bold;
+        font-size: 90px;
+        line-height: 150px;
+      }
+
+      .author {
+        position: absolute;
+        bottom: 35px;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        font-size: 76px;
+        line-height: 87px;
+        z-index: 100;
+
+      }
+
+      .author::after {
+        content: "";
+        position: absolute;
+        bottom: -22px;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        height: 10px;
+        width: 100%;
+        background-color: #08a53f;
+        border-radius: 21px;
+      }
+
+      #btn {
+        margin: 20px 0 0 20px;
+      }
+
     </style>
       <body>
         <div class="post-container">
-          <img src="./assets/serverImages/example.jpg" class="img">
+          <img src={{picture}} class="img">
           <div class="text">
-              <p>${req.body.message}</p>
+              <p>{{author}}</p>
           </div>
-          <div class="author">${req.body.name}</div>
+          <div class="author">{{message}}</div>
         </div>
       </body>
-    </html>`
-  })
-    .then(() => console.log('The image was created successfully!'))
+    </html>`,
+
+    content: {
+      author: req.body.message,
+      message: req.body.name,
+      picture: `http://127.0.0.1:3000/assets/serverImages/${req.file.originalname}`
+    }
+  }).then(() => console.log('The image was created successfully!'))
 });
 
 
