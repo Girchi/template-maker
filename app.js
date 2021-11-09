ef5718303a37accea6aab29d2e46678219526c6f
nNabakhteveli <nabakhtevelin@gmail.com>
2021-11-09 23:16:27 +0400

    Fixed image uploading issue with multer

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express, { response } from "express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import multer from 'multer';
+import bodyParser from "body-parser";
 import nodeHtmlToImage from 'node-html-to-image';
 import fs from 'fs';
 import request from 'request';
@@ -9,11 +10,12 @@ import request from 'request';
 
 const app = express();
 const fileStorageEngine = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './assets/serverImages')
+  destination(req, file, cb) {
+    cb(null, './assets/serverImages/')
   },
-    filename: (req, file, cb) => {
-      cb(null, file.originalname);
+    filename(req, file, cb) {
+      console.log("MULTER", file);
+      cb(null, `${file.originalname}`);
     }
   }
 );
@@ -21,26 +23,30 @@ const fileStorageEngine = multer.diskStorage({
 const upload = multer({storage: fileStorageEngine});
 const port = 3000;
 const hostname = "127.0.0.1";
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.set("view engine", "pug");
 app.use("/assets", express.static("assets"));
+app.use("/generate", express.static("generate"));
 
 app.get("/", upload.single("image"), (req, res) => {
+  // res.render(__dirname + "/snippet/create-post");
   res.render(__dirname + "/snippet/create-post", {qs: req.query, image: `./assets/serverImages/image.jpg`})
 });
 
 
-app.get("/post", upload.single("image"), (req,res) => {
-  console.log(req.query);
+app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
+  console.log("BODY BLIN", req.body);
   res.render(__dirname + '/snippet/post-success', {
-    qs: req.query,
-    author: req.query.author,
-    message: req.query.message,
+    qs: req.body,
+    author: req.body.author,
+    message: req.body.message,
     image: `./assets/serverImages/image.jpg`,
     generatedImage: "./assets/post/generatedTemplate/image.jpg"
   });
+
 
   async function download(url, path, callback) {
     request.head(url, (err, res, body) => {
@@ -50,12 +56,6 @@ app.get("/post", upload.single("image"), (req,res) => {
     })
   }
   
-  const urlToDownloadFrom = 'https://www.befunky.com/images/prismic/b60244c7-087b-409a-961b-831999aa5085_llama.jpg?auto=webp&format=jpg&width=1920&height=1920&fit=bounds'
-  const pathForImage = './assets/serverImages/image.jpg'
-  
-  download(urlToDownloadFrom, pathForImage, () => {
-    console.log('Image upload done! path=./assets/serverImages')
-  })
 
   nodeHtmlToImage({
     output: './assets/post/generatedTemplate/image.jpg',
@@ -77,7 +77,7 @@ app.get("/post", upload.single("image"), (req,res) => {
         margin: 50px 0;
         left: 50%;
         transform: translateX(-50%);
-        font-family: "BPG Nino Mtavruli Bold", sans-serif;
+        font-family: "BPG Nino Mtavruli", sans-serif;
         text-align: center;
         position: relative;
         color: white;
