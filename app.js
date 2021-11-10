d2fc014486968f14dbdd588367765bf9b03423e9
nNabakhteveli <nabakhtevelin@gmail.com>
2021-11-10 15:05:32 +0400

    Changed structure of the app and added font2base64

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import { dirname } from "path";
 import multer from 'multer';
 import bodyParser from "body-parser";
 import cors from 'cors';
-import nodeHtmlToImage from 'node-html-to-image';
 import fs from 'fs';
+import nodeHtmlToImage from 'node-html-to-image';
+import font2base64 from 'node-font2base64';
 
 
 const app = express();
@@ -42,37 +43,44 @@ app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
     author: req.body.author,
     message: req.body.message,
     image: `./assets/serverImages/${req.file.originalname}`,
-    generatedImage: "./assets/post/generatedTemplate/image.jpg"
+    generatedImage: "./assets/generatedTemplate/image.jpg"
   });
 
 
+  // DataURI to read uploaded image
   const image = fs.readFileSync(`./assets/serverImages/${req.file.originalname}`,);
   const base64Image = new Buffer.from(image).toString('base64');
   const dataURI = 'data:image/jpeg;base64,' + base64Image;
+  
+  // base64 urls for fonts
+  const FiraGo_Bold_Font = font2base64.encodeToDataUrlSync('./assets/css/fonts/FiraGO-Bold.otf');
+  const FiraGo_ExtraBold_Font = font2base64.encodeToDataUrlSync('./assets/css/fonts/FiraGO-ExtraBold.otf');
 
   nodeHtmlToImage({
-    output: './assets/post/generatedTemplate/image.jpg',
+    output: './assets/generatedTemplate/image.jpg',
     html: `
     <html>
+      <link rel="stylesheet" href="./assets/css/generatedTemplate.css">
+
     <style>
       @import url("//cdn.web-fonts.ge/fonts/bpg-nino-mtavruli/css/bpg-nino-mtavruli.min.css");
-
+      
       @font-face {
         font-family: "FiraGO-Bold";
-        src: url("http://127.0.0.1:3000/assets/post/css/fonts/FiraGO-Bold.otf");
+        src: url(${FiraGo_Bold_Font}) format('woff2');
       }
       
       @font-face {
         font-family: "FiraGO-ExtraBold";
-        src: url("http://127.0.0.1:3000/assets/post/css/fonts/FiraGO-ExtraBold.otf");
+        src: url(${FiraGo_ExtraBold_Font}) format('woff2');
       }
-
+    
       body {
         overflow: hidden;
         width: 2048px;
         height: 2048px;
       }
-
+    
       .post-container {
         width: 2048px;
         height: 2048px;
@@ -85,7 +93,7 @@ app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
         color: white;
         overflow: hidden;
       }
-
+    
       .post-container::after {
         content: "";
         position: absolute;
@@ -98,18 +106,18 @@ app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
         filter: blur(2px);
         pointer-events: none;
       }
-
+    
       .content {
         margin-top: -45px;
       }
-
+    
       .img {
         width: 100%;
         height: 100%;
         object-fit: cover;
         position: relative;
       }
-
+    
       .text {
         width: 100%;
         height: 90%;
@@ -121,7 +129,7 @@ app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
         align-items: flex-end;
         z-index: 100;
       }
-
+    
       .text p {
         font-family: "FiraGO-ExtraBold";
         width: 1565px;
@@ -132,7 +140,7 @@ app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
         font-weight: 800;
         text-align: center;
       }
-
+    
       .author {
         position: absolute;
         bottom: 50px;
@@ -147,7 +155,7 @@ app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
         color: #191919;
         font-weight: bold;
       }
-
+    
       .rectangle {
         width: 370px;
         height: 90px;
@@ -167,10 +175,11 @@ app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
         right: 20%; 
         bottom:20%
       }
-
+    
       #btn {
         margin: 20px 0 0 20px;
       }
+      
 
     </style>
       <body>
@@ -181,7 +190,7 @@ app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
           </div>
           <div class="author">{{author}}</div>
           <div class='rectangle'>
-            <img class="girchi-logo-rect" src='http://localhost:3000/assets/post/img/girchi-logo.png'>
+            <img class="girchi-logo-rect" src='http://localhost:3000/assets/img/girchi-logo.png'>
           </div
         </div>
       </body>
